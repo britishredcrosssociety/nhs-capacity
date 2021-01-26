cde9e218fb3d3b358aa40d848cafc969b12e0793
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-01-26 17:49:22 +0100

    Scrape A&E and ambulance data

---
+++ scrape/nhs-england.R
@@ -0,0 +1,26 @@
+# ---- Load libraries ----
+library(tidyverse)
+library(httr)
+library(readxl)
+
+# ---- A&E Attendance ----
+GET(
+  "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/01/Monthly-AE-December-2020.csv",
+  write_disk(tf <- tempfile(fileext = ".csv"))
+)
+
+a_and_e <- read_csv(tf)
+
+unlink(tf)
+rm(tf)
+
+# ---- Ambulance Quality Indicators ----
+GET(
+  "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/01/AmbSYS-December-2020.xlsx",
+  write_disk(tf <- tempfile(fileext = ".xlsx"))
+)
+
+ambulance <- read_excel(tf, sheet = "Response Times")
+
+unlink(tf)
+rm(tf)
