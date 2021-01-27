e8b797b0d1124f2dfcf496320c5cbaa80e2c5af6
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-01-27 10:31:06 +0100

    Add ambulance quality indicators

--- scrape/nhs-england.R
+++ scrape/nhs-england.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(httr)
 library(readxl)
+library(janitor)
 
 # ---- A&E Attendance ----
 GET(
@@ -9,7 +10,7 @@ GET(
   write_disk(tf <- tempfile(fileext = ".csv"))
 )
 
-a_and_e <- read_csv(tf)
+eng_a_and_e <- read_csv(tf)
 
 unlink(tf)
 rm(tf)
@@ -20,7 +21,54 @@ GET(
   write_disk(tf <- tempfile(fileext = ".xlsx"))
 )
 
-ambulance <- read_excel(tf, sheet = "Response Times")
+# column names and types to use for loading all data
+ambo_colnames  <- c("Code", "Ambulance Service", "Count of Incidents", "Blank", "Total (hours)","Mean (min:sec)", "90th centile (min:sec))")
+ambo_types <- c("text", "text", "numeric", "numeric", "numeric", "date", "date")
+
+
+# Category 1 
+eng_ambo_cat1 <- read_excel(tf,
+                           sheet = "Response Times", range = "C8:I18", 
+                           col_names = ambo_colnames, col_types = ambo_types) %>% 
+  remove_empty("cols") %>% 
+  mutate(category = "cat1")
+
+# Category 1T
+eng_ambo_cat1t <- read_excel(tf,
+                            sheet = "Response Times", range = "C22:I32", 
+                            col_names = ambo_colnames, col_types = ambo_types) %>% 
+  remove_empty("cols") %>% 
+  mutate(category = "cat1t")
+
+# Category 2
+eng_ambo_cat2 <- read_excel(tf,
+                           sheet = "Response Times", range = "C36:I46", 
+                           col_names = ambo_colnames, col_types = ambo_types) %>% 
+  remove_empty("cols") %>% 
+  mutate(category = "cat2")
+
+# Category 3
+eng_ambo_cat3 <- read_excel(tf,
+                           sheet = "Response Times", range = "C50:I60", 
+                           col_names = ambo_colnames, col_types = ambo_types) %>% 
+  remove_empty("cols") %>% 
+  mutate(category = "cat3")
+
+# Category 4
+eng_ambo_cat4 <- read_excel(tf,
+                           sheet = "Response Times", range = "C64:I74", 
+                           col_names = ambo_colnames, col_types = ambo_types) %>% 
+  remove_empty("cols") %>% 
+  mutate(category = "cat4")
+
+# combine stats
+eng_ambo <- bind_rows(
+  eng_ambo_cat1,
+  eng_ambo_cat1t,
+  eng_ambo_cat2,
+  eng_ambo_cat3,
+  eng_ambo_cat4
+)
 
 unlink(tf)
 rm(tf)
