f7cc1cb44a9f4ec70b48a119973312000f54a1f7
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-03-05 14:11:13 +0100

    Add note to consider crosstalk

--- app/app.R
+++ app/app.R
@@ -1,3 +1,11 @@
+# Consider using crosstalk: https://github.com/rstudio/crosstalk
+
+
+
+
+
+
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
