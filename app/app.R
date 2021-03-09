a56eeb85144a45efe969edc0373bcabb90a08bd0
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-03-09 17:58:35 +0100

    Load data and initiate structure

--- app/app.R
+++ app/app.R
@@ -1,57 +1,60 @@
-# Consider using crosstalk: https://github.com/rstudio/crosstalk
-
-
-
-
-
-
-
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# ---- Load libraries ----
 library(shiny)
+library(sf)
+library(dplyr)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# ---- Load data sets ----
+points_trusts <-
+  read_sf("data/points_nhs_trusts.geojson")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+ae <-
+  readRDS("data/ae.rds")
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+beds_days <-
+  readRDS("data/beds_days.rds")
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+beds_nights <-
+  readRDS("data/beds_nights.rds")
+
+cancer_wait_times <-
+  readRDS("data/cancer_wait_times.rds")
+
+diagnostic_wait_times <-
+  readRDS("data/diagnostic_wait_times.rds")
+
+outpatient_referrals <-
+  readRDS("data/diagnostic_wait_times.rds")
+
+rtt <-
+  readRDS("data/referral_treatment_waiting_times.rds")
+
+# ---- UI ----
+ui <- navbarPage(
+
+  # - Title -
+  title = "NHS Capacity Analysis",
+
+  # - Tab Panels -
+  # Explore Home Page
+  tabPanel(
+    title = "Explore",
+    
+    fluidPage(
+      
     )
+    
+  ),
+  
+  # User Guide
+  tabPanel(
+    "Guide"
+  )
 )
 
-# Define server logic required to draw a histogram
+# ---- Server ----
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
