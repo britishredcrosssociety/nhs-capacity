855b6a08bf1c60209f5fe0740d09424d18f01e02
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-03-09 12:16:55 +0100

    Save A&E data to app

---
+++ analyse/match-indicators-to-points.R
@@ -0,0 +1,50 @@
+# ---- Libraries ----
+library(tidyverse)
+library(sf)
+
+# ---- Load data ----
+# Load indicators into global env
+file_path <- "data/"
+
+file_names <-
+  list.files(file_path) %>%
+  .[str_detect(., "^nhs_")]
+
+file_names %>%
+  map(
+    function(file_name) {
+      assign(
+        x = str_remove(file_name, ".csv"),
+        value = read_csv(paste0(file_path, file_name)),
+        envir = .GlobalEnv
+      )
+    }
+  )
+
+# Load Trust geometries
+points_trusts <-
+  read_sf("data/points_nhs_trusts.geojson")
+
+# Keep only open trusts to match to indicators
+open_trusts <-
+  points_trusts %>%
+  as_tibble() %>% 
+  filter(status == "open") %>% 
+  select(
+    org_name,
+    org_code
+  )
+
+# ---- Match indicators to open trusts and save to app ----
+# AE
+open_trusts %>% 
+  left_join(
+    nhs_ae,
+    by = "org_code"
+  ) %>%
+  select(-name) %>% 
+  mutate(
+    org_name = str_to_title(org_name),
+    org_name = str_replace(org_name, "Nhs", "NHS")
+  ) %>% 
+  write_rds("app/data/ae.rds")
