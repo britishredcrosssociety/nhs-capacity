deeffd76d21c3f4d2dcd1ce99cf230e433145f91
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-03-09 11:40:37 +0100

    Refactor NHS trusts code

--- analyse/geocode-nhs-trusts.R
+++ analyse/geocode-nhs-trusts.R
@@ -1,66 +1,10 @@
 # ---- Load libraries ----
 library(tidyverse)
-library(readxl)
-library(httr)
-library(tidygeocoder)
+library(sf)
+library(geographr)
 
-# ---- Load data ----
-# Source:
-# - https://digital.nhs.uk/services/organisation-data-service/data-downloads/other-nhs-organisations
-# - see page 85 of the Standards Repository Document for metadata
-
-# Other potential (unused) sources of Trusts:
-# - https://www.england.nhs.uk/publication/nhs-provider-directory/
-# - https://odsportal.digital.nhs.uk/Organisation/Search
-# - https://www.nhs.uk/ServiceDirectories/Pages/NHSTrustListing.aspx
-
-# Load raw trust data
-GET(
-  "https://files.digital.nhs.uk/assets/ods/current/etr.zip",
-  write_disk(tf <- tempfile(fileext = ".zip"))
-)
-unzip(tf, exdir = tempdir())
-unlink(tf)
-
-trusts_raw <- read_csv(
-  file = list.files(
-    tempdir(),
-    pattern = "*.csv",
-    full.names = TRUE
-  ),
-  col_names = c(
-    "org_code",
-    "name",
-    "national_grouping",
-    "high_level_health_geography",
-    "address_1",
-    "address_2",
-    "address_3",
-    "address_4",
-    "address_5",
-    "postcode",
-    "open_date",
-    "close_date"
-  )
-)
-
-# ---- Wrangle ----
-# Create open status
-trusts_clean <-
-  trusts_raw %>%
-  mutate(status = if_else(is.na(close_date), "open", "closed")) %>%
-  select(
-    -open_date,
-    -close_date
-  )
-
-# ---- Geocode ----
-trusts_geocoded <-
-  trusts_clean %>%
-  geocode(
-    postalcode = postcode,
-    method = "osm"
-  )
-
-trusts_geocoded %>%
-  write_csv("data/nhs-trusts-geocoded.csv")
+# ---- Load and Save data ----
+# The geographr package is large, and should not be called into a Shiny app.
+# Save the NHS Trust points.
+points_nhs_trusts %>% 
+  write_sf("data/points_nhs_trusts.geojson")
