2f52fffc11c7a2514000d0b42b5ab79cd5e5a289
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-04-14 12:39:25 +0200

    Update Trusts to only include England.

--- analyse/nhs-england-trusts-geocode.R
+++ analyse/nhs-england-trusts-geocode.R
@@ -13,6 +13,8 @@ points_nhs_trusts %>%
     org_code = nhs_trust_code,
     org_name = nhs_trust_name
   ) %>%
+  # Remove Welsh Trusts
+  filter(org_code != "RQF" & org_code != "RT4" & org_code != "RYT") %>%
   write_sf("data/points_nhs_trusts.geojson")
 
 # Save only open trusts to app
@@ -22,11 +24,13 @@ open_trusts <-
     org_code = nhs_trust_code,
     org_name = nhs_trust_name
   ) %>%
+  # Remove Welsh Trusts
+  filter(org_code != "RQF" & org_code != "RT4" & org_code != "RYT") %>%
   filter(status == "open") %>%
   mutate(
     org_name = str_to_title(org_name),
     org_name = str_replace(org_name, "Nhs", "NHS")
   )
 
-open_trusts %>% 
+open_trusts %>%
   write_sf("app/data/open_nhs_trusts_points.geojson")
