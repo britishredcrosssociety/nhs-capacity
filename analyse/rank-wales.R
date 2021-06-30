a509d5e2d482f794695ad5d41dce00d5d710f4ad
Matt Thomas <matthewgthomas@gmail.com>
2021-06-30 13:36:17 +0100

    Update rank-wales.R

--- analyse/rank-wales.R
+++ analyse/rank-wales.R
@@ -56,68 +56,17 @@ wales <-
 
 # ---- Find worst-performing Health Boards across all metrics ----
 # Bin each metric into quintiles and look for HBs in the worst-performing quintile across multiple metrics
-wales %>% 
+wales_performance <- 
+  wales %>% 
   mutate(
     ae_bin = quantise(`Percentage breached`, num_quantiles = 5, highest_quantile_worst = TRUE),
     ambo_bin = quantise(`Red calls - % of emergency responses arriving at the scene within 8 minutes`, num_quantiles = 5, highest_quantile_worst = FALSE),
-    beds_bin = quantile(`% general and acute beds occupied`, num_quantiles = 5, highest_quantile_worst = TRUE),
+    beds_bin = quantise(`% general and acute beds occupied`, num_quantiles = 5, highest_quantile_worst = TRUE),
     cancer_bin = quantise(`% starting treatment within 62 days`, num_quantiles = 5, highest_quantile_worst = FALSE),
     rtt_bin = quantise(`% waiting 53+ weeks`, num_quantiles = 5, highest_quantile_worst = TRUE)
-  )
-  
-
-beds_binned <- 
-  beds %>% 
-  filter(str_detect(name, "^% Total Day")) %>% 
-  drop_na(value) %>% 
-  
-  group_by(name) %>% 
-  mutate(bin = quantise(value, num_quantiles = 5)) %>% 
-  ungroup() %>% 
-  
-  select(trust_code = `Trust Code`, beds_bin = bin)
-
-cancer_binned <- 
-  cancer_wait_times %>% 
-  mutate(value = Breaches / `Total Treated`) %>%
-  rename(name = Standard) %>%
-  
-  filter(name == "62 Days") %>% 
-  drop_na(value) %>% 
-  
-  group_by(name) %>% 
-  mutate(bin = quantise(value, num_quantiles = 5)) %>% 
-  ungroup() %>% 
-  
-  select(trust_code = `Trust Code`, cancer_bin = bin)
-
-diagnostic_wait_times
-
-rtt_binned <- 
-  rtt %>% 
-  filter(
-    `Referral Treatment Type` == "Incomplete Pathways" &
-      str_detect(name, "^Waiting 52+")
   ) %>% 
   
-  drop_na(value) %>% 
-  
-  group_by(name) %>% 
-  mutate(bin = quantise(value, num_quantiles = 5)) %>% 
-  ungroup() %>% 
-  
-  select(trust_code = `Trust Code`, rtt_bin = bin)
-
-# Calculate overall Trust performance, based on the quintiles
-trust_performance <- 
-  open_trusts %>% 
-  rename(trust_name = org_name, trust_code = org_code) %>% 
-  
-  left_join(ae_binned, by = "trust_code") %>% 
-  left_join(beds_binned, by = "trust_code") %>% 
-  left_join(cancer_binned, by = "trust_code") %>% 
-  left_join(rtt_binned, by = "trust_code") %>% 
-  
+  # Calculate overall Trust performance, based on the quintiles
   rowwise() %>%
   mutate(bin_sum = sum(c_across(ae_bin:rtt_bin), na.rm = TRUE)) %>% 
   ungroup() %>% 
