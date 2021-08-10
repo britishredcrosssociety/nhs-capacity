e5d99e8fb7c57b0996717cf7f91f96768e43127d
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-08-10 17:20:29 +0200

    Refactor diagnostic waiting time data

---
+++ preprocess/england/diagnostic-wait-times.R
@@ -0,0 +1,75 @@
+# Load libs
+library(tidyverse)
+library(httr)
+library(readxl)
+library(sf)
+library(geographr)
+library(usethis)
+
+# Create trust lookup of open trusts
+open_trusts <-
+  points_nhs_trusts |>
+  as_tibble() |>
+  filter(status == "open") |>
+  select(
+    `Trust Code` = nhs_trust_code,
+    `Trust Name` = nhs_trust_name
+  ) |>
+  mutate(
+    `Trust Name` = str_to_title(`Trust Name`),
+    `Trust Name` = str_replace(`Trust Name`, "Nhs", "NHS")
+  )
+
+# Load raw data
+GET(
+  "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/07/Monthly-Diagnostics-Web-File-Provider-May-2021_84CSC.xls",
+  write_disk(tf <- tempfile(fileext = ".xls"))
+)
+
+raw <-
+  read_excel(
+    tf,
+    sheet = "Provider",
+    skip = 13
+  )
+
+# Remove first two rows (summary & blank)
+diagnostics_sliced <-
+  raw |>
+  slice(-(1:2))
+
+# Select cols
+diagnostics_vars <-
+  diagnostics_sliced |>
+  select(
+    `Trust Code` = `Provider Code`,
+    `Waiting List Total` = `Total Waiting List`,
+    `Waiting 6+ weeks` = `Number waiting 6+ Weeks`,
+    `Waiting 13+ weeks` = `Number waiting 13+ Weeks`
+  )
+
+# Calculate relative scores
+diagnostics_scores <-
+  diagnostics_vars |>
+  mutate(
+    `% waiting 6+ weeks` = `Waiting 6+ weeks` / `Waiting List Total`,
+    `% waiting 13+ weeks` = `Waiting 13+ weeks` / `Waiting List Total`
+  )
+
+# Filter to only open trusts
+diagnostics_open <-
+  open_trusts |>
+  left_join(
+    diagnostics_scores,
+    by = "Trust Code"
+  )
+
+# Pivot longer
+diagnostic_wait_times <-
+  diagnostics_open|>
+  pivot_longer(
+    cols = !starts_with("Trust")
+  )
+
+# Save
+use_data(diagnostic_wait_times, overwrite = TRUE)
