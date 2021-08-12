a0d82714570e6acc1e553eed9d8c320e271dd401
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-08-12 15:59:55 +0200

    Refactor A&E Wales data

---
+++ preprocess/wales/ae.R
@@ -0,0 +1,57 @@
+# ---- Load ----
+library(jsonlite)
+library(httr)
+library(RCurl)
+library(dplyr)
+library(readr)
+library(tidyr)
+library(lubridate)
+library(usethis)
+
+# Function to scrape Wales data
+download.wales <- function(url) {
+  # load initial data
+  wimd_curr <- fromJSON(getURL(url), flatten = T)
+
+  # put data into data.frame
+  wimd_dat <- wimd_curr$value
+
+  # get url of the first next page
+  next_page <- wimd_curr$odata.nextLink
+
+  # loop over the .json pages until we run out of data
+  while (!is.null(next_page)) {
+    wimd_curr <- fromJSON(getURL(next_page), flatten = T) # download next batch of data
+
+    wimd_dat <- bind_rows(wimd_dat, wimd_curr$value) # append to data.frame
+    next_page <- wimd_curr$odata.nextLink # get url of next page (if there is one)
+
+    print(next_page) # track progress
+  }
+
+  wimd_dat
+}
+
+# ---- Wrangle ----
+raw <- download.wales("http://open.statswales.gov.wales/en-gb/dataset/hlth0035")
+
+# keep only latest stats
+ae <-
+  raw |>
+  mutate(Date_SortOrder = as.integer(Date_SortOrder)) |>
+  filter(Date_SortOrder == max(Date_SortOrder))
+
+# save wide-format version of the data
+wales_ae <-
+  ae |>
+  filter(Target_Code == "4hr") |>
+  filter(!Hospital_ItemName_ENG %in% c("Major emergency departments", "Other emergency departments/minor injury units")) |>
+  select(
+    Hospital_code = Hospital_Code,
+    Hospital = Hospital_ItemName_ENG,
+    Measure_Code, Data
+  ) |>
+  distinct() |>
+  pivot_wider(names_from = Measure_Code, values_from = Data)
+
+use_data(wales_ae, overwrite = TRUE)
