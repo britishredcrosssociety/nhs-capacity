7a5a2fcb41fbbd514d692b1e5a69351ac5282bfd
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-09-16 13:01:44 +0200

    Remove sum of 5 calculations and compute ranks and raw scores

--- preprocess/england/performance.R
+++ preprocess/england/performance.R
@@ -6,9 +6,6 @@ library(sf)
 # ---- Load funs ----
 source("https://github.com/britishredcrosssociety/resilience-index/raw/main/R/utils.R")
 
-# Return 1 if `x` is in the worst-performing quintile
-count_if_worst <- function(x, q = 5) ifelse(!is.na(x) & x == q, 1, 0)
-
 # Inverse rank - override imported to make NA's last
 inverse_rank <- function(x) (length(x) + 1) - rank(x, na.last = "keep")
 
@@ -104,8 +101,7 @@ complete_data <-
   select(-number_missing)
 
 # ---- Find worst-performing Trusts across all metrics ----
-# Bin each metric into quintiles and look for HBs in the worst-performing
-# quintile across multiple metrics
+# Lower rank (e.g., 2 is lower than 1) = lower (worse) rank
 ranks <-
   complete_data |>
   mutate(
@@ -122,92 +118,64 @@ ranks <-
     ends_with("_rank")
   )
 
-bins <-
+ranks_sum <-
   ranks |>
-  mutate(
-    ae_bin = quantise(ae_rank, num_quantiles = 5),
-    beds_night_bin = quantise(beds_night_rank, num_quantiles = 5),
-    beds_day_bin = quantise(beds_day_rank, num_quantiles = 5),
-    cancer_bin = quantise(cancer_rank, num_quantiles = 5),
-    diagnostic_bin = quantise(diagnostic_rank, num_quantiles = 5),
-    rtt_bin = quantise(rtt_rank, num_quantiles = 5)
-  ) |>
-  select(
-    `Trust Code`,
-    stp_code,
-    ends_with("_bin")
-  )
-
-bins_sum <-
-  bins |>
   rowwise() |>
   mutate(
-    bin_sum = sum(
+    rank_sum = sum(
       c_across(!c(`Trust Code`, stp_code)),
       na.rm = TRUE
     )
   ) |>
   ungroup() |>
   mutate(
-    overall_rank = rank(bin_sum),
-    overall_bin = quantise(overall_rank, num_quantiles = 5)
+    overall_rank = rank(rank_sum)
   ) |>
-  arrange(desc(overall_rank))
-
-bins_worst_count <-
-  bins_sum |>
-  mutate(
-    sum_of_5s = count_if_worst(ae_bin) +
-      count_if_worst(beds_night_bin) +
-      count_if_worst(beds_day_bin) +
-      count_if_worst(cancer_bin) +
-      count_if_worst(diagnostic_bin) +
-      count_if_worst(rtt_bin)
-  )
-
-trust_performance <-
-  bins_worst_count |>
-  select(
-    `Trust Code`,
-    `A&E performance (5 = worst performing)` = ae_bin,
-    `Bed night occupancy performance (5 = worst performing)` = beds_night_bin,
-    `Bed day occupancy performance (5 = worst performing)` = beds_day_bin,
-    `Cancer waiting list performance (5 = worst performing)` = cancer_bin,
-    `Diagnostic waiting time performance (5 = worst performing)` = diagnostic_bin,
-    `RTT performance (5 = worst performing)` = rtt_bin,
-    `Overall performance (5 = worst performing)` = overall_bin,
-    `No. of services (out of 6) scoring worst in performance` = sum_of_5s
-  )
+  arrange(desc(overall_rank)) |>
+  select(-rank_sum)
 
 # ---- Find worst-performing STP's ----
-# Filter to the worst performing Trust in each STP. Use rank as the first
-# criteria, followed by number of services scoring worst in performance
-# (sum_of_5s)
+# Filter to worst performing Trust in each STP.
 stp_worst <-
-  bins_worst_count |>
+  ranks_sum |>
   group_by(stp_code) |>
-  arrange(desc(overall_rank), desc(sum_of_5s)) |>
+  arrange(desc(overall_rank)) |>
   slice(1) |>
-  ungroup()
+  ungroup() |>
+  arrange(desc(overall_rank))
 
-stp_performance <-
+# ---- Rank STP's ----
+stp_rank <-
   stp_worst |>
-  select(
-    stp_code,
-    `A&E performance (5 = worst performing)` = ae_bin,
-    `Bed night occupancy performance (5 = worst performing)` = beds_night_bin,
-    `Bed day occupancy performance (5 = worst performing)` = beds_day_bin,
-    `Cancer waiting list performance (5 = worst performing)` = cancer_bin,
-    `Diagnostic waiting time performance (5 = worst performing)` = diagnostic_bin,
-    `RTT performance (5 = worst performing)` = rtt_bin,
-    `Overall performance (5 = worst performing)` = overall_bin,
-    `No. of services (out of 6) scoring worst in performance` = sum_of_5s
+  mutate(
+    across(
+      !c(`Trust Code`, stp_code),
+      rank
+    )
   )
+
+stp_rank_renamed <-
+  stp_rank |>
+  rename(
+    `A&E rank` = ae_rank,
+    `Bed night occupancy rank` = beds_night_rank,
+    `Bed day occupancy rank` = beds_day_rank,
+    `Cancer waiting list rank` = cancer_rank,
+    `Diagnostic waiting time rank` = diagnostic_rank,
+    `RTT rank` = rtt_rank,
+    `Overall rank` = overall_rank
+  )
+
+# ---- Join raw (complete) data back to ranks ----
+ranks_and_raw <-
+  stp_rank_renamed |>
+  left_join(complete_data) |>
+  select(-`Trust Code`)
 
 # ---- Join boundary data ----
 england_performance <-
   boundaries_stp |>
-  left_join(stp_performance)
+  left_join(ranks_and_raw)
 
 england_performance |>
-write_rds("preprocess/data/england_performance.rds")
+  write_rds("preprocess/data/england_performance.rds")
