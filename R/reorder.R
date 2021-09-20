808b3131c079f21f529c6aef9c89dc150ac3df1b
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-09-20 10:54:07 +0200

    Add factor reording funs

---
+++ R/reorder.R
@@ -0,0 +1,16 @@
+# No license on code: note this same code has been reused in the tidytext
+# package  (excluded here to reduce dependencies/size).
+reorder_within <- function(x, by, within, fun = mean, sep = "___", ...) {
+  new_x <- paste(x, within, sep = sep)
+  stats::reorder(new_x, by, FUN = fun)
+}
+
+scale_x_reordered <- function(..., sep = "___") {
+  reg <- paste0(sep, ".+$")
+  ggplot2::scale_x_discrete(labels = function(x) gsub(reg, "", x), ...)
+}
+
+scale_y_reordered <- function(..., sep = "___") {
+  reg <- paste0(sep, ".+$")
+  ggplot2::scale_y_discrete(labels = function(x) gsub(reg, "", x), ...)
+}
