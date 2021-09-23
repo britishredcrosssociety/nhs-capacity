5375a67ce09b9fde9e3d3b953e7e33947ba88095
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-09-23 18:48:14 +0200

    Add raw IAPT data

---
+++ preprocess/england/iapt.R
@@ -0,0 +1,44 @@
+# Load libs
+library(tidyverse)
+library(httr)
+
+# Load data
+# Download latest quarterly data by STP/ICS from https://digital.nhs.uk/data-and-information/publications/statistical/psychological-therapies-report-on-the-use-of-iapt-services
+GET(
+  "https://files.digital.nhs.uk/F4/95AF18/iapt_quarterly_STP_and_CCGs_q1_2122.zip",
+  write_disk(tf <- tempfile(fileext = ".zip"))
+)
+
+unzip(tf, exdir = tempdir())
+
+iapt_raw <-
+  read_csv(
+    file.path(tempdir(), "iapt_quarterly_STP_and_CCGs_q1_2122.csv"),
+    col_types = cols(.default = col_character())
+  )
+
+# Analyse waiting times by STP
+iapt_filtered <-
+  iapt_raw |>
+  filter(
+    CCG == "All_CCG",
+    VariableType == "Total",
+    GroupType == "STP",
+    STP != "InvalidCode",
+    MEASURE_NAME == "Percentage_FirstTreatment18WeeksFinishedCourseTreatment"
+  )
+
+iapt_selected <-
+  iapt_filtered |>
+  select(
+    stp_code = STP,
+    variable = MEASURE_NAME,
+    score = MEASURE_VALUE_SUPPRESSED
+  )
+
+england_iapt <-
+  iapt_selected
+
+# Save
+england_iapt |>
+  write_rds("preprocess/data/england_iapt.rds")
